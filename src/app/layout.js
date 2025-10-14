15e53014512fdb1181fc05e9cba8b75e8cfe5844
JOAO PAULO SCHAUSTECK <iauxcoders@gmail.com>
2025-10-14 20:14:11 -0300

    chore(analytics): remover GTM e noscript; manter apenas GA4 via gtag.js (beforeInteractive)

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,31 +23,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased future-grid-bg m-0`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NFXXFG6H"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1QTM69VDTQ"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         <Script
           id="ga-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-1QTM69VDTQ');`,
-          }}
-        />
-        <Script
-          id="gtm-init"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html:
-              "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NFXXFG6H');",
+            __html: `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-1QTM69VDTQ');`,
           }}
         />
         <div className="mx-auto w-full max-w-[1440px]">
